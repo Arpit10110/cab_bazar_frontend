371aff4963b304f69f9abde99329db2d6993b0f6
Arpit <omagrahari55@gmail.com>
2026-03-27 23:32:16 +0530

    added the hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './component/Home'
 import Promo from './component/Promo/Promo'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cablist" element={<Promo />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cablist" element={<Promo />} />
+    </Routes>
   )
 }
 
